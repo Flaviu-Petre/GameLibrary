3403bc7ddf1006ceb3fb993a0ac6a949f2842d43
Flaviu-Petre <flaviu.petre09@gmail.com>
2025-12-02 21:53:55 +0200

    [Add] common DTO classes for game library
    Introduced DeveloperDto, GameDto, GenreDto, PlatformDto, PublisherDto, and UserDto to represent core entities in the game library domain. These DTOs will be used for data transfer between layers and support relationships among games, users, developers, publishers, genres, and platforms.

---
+++ GameLibrary.API/GameLibrary.Core/Dtos/Common/GameDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibrary.Core.Dtos.Common
+{
+    public class GameDto
+    {
+        public string? Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string? Description { get; set; }
+        public List<UserDto>? Users { get; set; }
+        public DeveloperDto? Developer { get; set; }
+        public List<GenreDto>? Genres { get; set; }
+        public PublisherDto? Publisher { get; set; }
+    }
+}
---
+++ GameLibrary.API/GameLibrary.Core/Dtos/Common/PublisherDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibrary.Core.Dtos.Common
+{
+    public class PublisherDto
+    {
+        public string? Name { get; set; }
+        public string? Country { get; set; }
+        public List<GameDto>? Games { get; set; }
+    }
+}
